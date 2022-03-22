3bdbe8347195ef14c1a68f569d5151e0042535ae
Ernest Wambua <ernestwambua2@gmail.com>
2022-03-22 16:13:17 +0300

    Moved around some files

---
+++ src/apis/mpesa.js
@@ -0,0 +1,25 @@
+const { default: axios } = require('axios');
+const { consumerKey, consumerSecret } = require('../config');
+
+const encodedString = Buffer.from(`${consumerKey}:${consumerSecret}`).toString(
+  'base64'
+);
+// const decodedString = Buffer.from(encodedString, "base64").toString("ascii");
+
+const authString = `Basic ${encodedString}`;
+
+exports.fetchAccessToken = async () => {
+  const res = await axios.get(
+    'https://sandbox.safaricom.co.ke/oauth/v1/generate',
+    {
+      headers: {
+        Authorization: authString,
+      },
+      params: {
+        grant_type: 'client_credentials',
+      },
+    }
+  );
+
+  return res.data.access_token;
+};
---
+++ src/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const { default: axios } = require("axios");
+
+const { fetchAccessToken } = require("./apis/mpesa");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success reached server successfully !",
+    url: req.url,
+  });
+});
+
+app.get("/mpesa/register-url", async (req, res) => {
+  const ngrokUrl = "http://c860ca78557f.ngrok.io";
+  const accessToken = await fetchAccessToken();
+  try {
+    const response = await axios.post(
+      "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl",
+      {
+        ValidationURL: `${ngrokUrl}/validation`,
+        ConfirmationURL: `${ngrokUrl}/confirmation`,
+        ResponseType: "Cancelled",
+        ShortCode: 174379,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send({ error: "Server error !" });
+    console.error(error);
+  }
+});
+
+app.get("/mpesa/c2b", async (req, res) => {
+  try {
+    const accessToken = await fetchAccessToken();
+    const response = await axios.post(
+      "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate",
+      {
+        CommandID: "CustomerPayBillOnline",
+        Amount: 100,
+        Msisdn: 254708374149,
+        BillRefNumber: 600000,
+        ShortCode: 174379,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          ContentType: "application/json",
+        },
+      }
+    );
+
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
+
+app.post("/validation", (req, res) => {
+  console.log(req.body);
+  res.status(200).send({});
+});
+
+app.post("/confirmation", (req, res) => {
+  console.log(req.body);
+  res.status(200).send({});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`Server Listening on PORT: ${PORT}`);
+});
